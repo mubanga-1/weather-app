81b65748c299c6ba0fa817f913fc3569f53b5a60
mubanga-1 <mubanga.s10@gmail.com>
2025-06-02 13:48:42 -0700

    Finish project setup

---
+++ webpack.common.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-undef */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'dist'),
+		clean: true,
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/template.html',
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.css/i,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(woff|woff2|ttf)$/i,
+				type: 'asset/resource',
+			},
+			{
+				test: /\.(png|jpeg|jpg)$/i,
+				type: 'asset/resource',
+			},
+		],
+	},
+};
