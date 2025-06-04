3bc612c50c929e683b4cff9de8a6e66b1efe4ee7
mubanga-1 <mubanga.s10@gmail.com>
2025-06-04 03:11:33 +0200

    Add proper configuration for prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
-import eslintPluginPretter from "eslint-plugin-prettier/recommended";
+import eslintPluginPretter from "eslint-plugin-prettier";
 
 export default defineConfig([
   {
