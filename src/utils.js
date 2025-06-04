e2e702371a6505c6a1f62f357e112b2226d968fe
mubanga-1 <mubanga.s10@gmail.com>
2025-06-04 03:12:34 +0200

    Add utils.js

---
+++ src/utils.js
@@ -0,0 +1,15 @@
+function createElement(elementObject) {
+  let newElement;
+
+  if (elementObject) {
+    newElement = document.createElement(elementObject.type);
+  }
+
+  newElement.id = elementObject.id;
+  newElement.classList = elementObject.classList;
+  newElement.textContent = elementObject.textContent;
+
+  return newElement;
+}
+
+export { createElement };
